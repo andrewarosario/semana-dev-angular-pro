8aafe381e019656a501b528b25c0e4b84b0c4359
andrewarosario <andrew.arosario@gmail.com>
2024-01-03 21:43:52 -0300

    feat: add products routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HelloWorldComponent,
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@ecommerce/products').then((c) => c.ProductsComponent),
+  },
+];
