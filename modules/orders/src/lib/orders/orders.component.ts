ee04522464e806422a52cd26432747e44c7a1846
andrewarosario <andrew.arosario@gmail.com>
2024-01-03 21:53:44 -0300

    feat: add shared ui lib

--- modules/orders/src/lib/orders/orders.component.ts
+++ modules/orders/src/lib/orders/orders.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SharedUiComponent } from '@ecommerce/shared-ui';
 
 @Component({
   selector: 'ecommerce-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedUiComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css',
 })
--- modules/products/src/lib/products/products.component.ts
+++ modules/products/src/lib/products/products.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SharedUiComponent } from '@ecommerce/shared-ui';
 
 @Component({
   selector: 'ecommerce-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedUiComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
