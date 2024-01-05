d581b356239528c5f0f77d179cf4589b98a599f2
andrewarosario <andrew.arosario@gmail.com>
2024-01-05 09:46:40 -0300

    feat: add product search

---
+++ modules/products/src/lib/models/product-search.ts
@@ -0,0 +1,25 @@
+export interface ProductSearch {
+  count: number;
+  next: unknown;
+  previous: unknown;
+  results: Product[];
+}
+
+export interface Product {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
--- modules/products/src/lib/products/products.component.ts
+++ modules/products/src/lib/products/products.component.ts
@@ -1,12 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { SharedUiComponent } from '@ecommerce/shared-ui';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable, debounceTime, filter, switchMap } from 'rxjs';
+import { Product } from '../models/product-search';
+import { ProductSearchService } from '../services/product-search/product-search.service';
 
 @Component({
   selector: 'ecommerce-products',
   standalone: true,
-  imports: [CommonModule, SharedUiComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  control = new FormControl('', { nonNullable: true });
+  products$!: Observable<Product[]>;
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit() {
+    this.products$ = this.control.valueChanges.pipe(
+      filter((text) => text.length > 1),
+      debounceTime(400),
+      switchMap((text) => this.productSearchService.searchByTerm(text))
+    );
+  }
+}
